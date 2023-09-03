f3f93bd8c9ab53455507838a04477bd389bff1fc
$ ducky#6170 <122984511+ducky-g9av@users.noreply.github.com>
2023-09-03 20:22:33 +0200

    Update extspoof.py
    fixed code

--- extspoof.py
+++ extspoof.py
@@ -100,25 +100,25 @@ def ctm(command):
     argv1 = command[1]
     argv2 = command[2]
 
-    
     input_data = argv2
     output_file = argv1
     # Encode the PDF file into a string
-    with open(input_data, "rb") as pdf_file:
-        encoded_string = base64.b64encode(pdf_file.read()).decode("utf-8")
-    
-    with open(output_file, "a") as new_code:
-        new_code.write("\n\nimport base64\n\n")
-        new_code.write("encoded_pdf = \"" + encoded_string + "\"\n\n")
-        new_code.write("# Decode the PDF string back into bytes\n")
-        new_code.write("decoded_pdf = base64.b64decode(encoded_pdf)\n\n")
-        new_code.write("# Write the decoded PDF bytes into a new file\n")
-        new_code.write("with open(\"decoded.pdf\", \"wb\") as pdf_file:\n")
-        new_code.write("    pdf_file.write(decoded_pdf)\n")
-    print("(ExtSpoof) - Success.")
+    try:
+        with open(input_data, "rb") as pdf_file:
+            encoded_string = base64.b64encode(pdf_file.read()).decode("utf-8")
+        
+        with open(output_file, "a") as new_code:
+            new_code.write("\n\nimport base64\n\n")
+            new_code.write("encoded_pdf = \"" + encoded_string + "\"\n\n")
+            new_code.write("# Decode the PDF string back into bytes\n")
+            new_code.write("decoded_pdf = base64.b64decode(encoded_pdf)\n\n")
+            new_code.write("# Write the decoded PDF bytes into a new file\n")
+            new_code.write("with open(\"decoded.pdf\", \"wb\") as pdf_file:\n")
+            new_code.write("    pdf_file.write(decoded_pdf)\n")
+        print("(ExtSpoof) - Success.")
 
-    else:
-        print("(ExtSpoof) - File was not found.")
+    except:
+        print("ExtSpoof - File was not found.")
 
 
 def extspoof():
